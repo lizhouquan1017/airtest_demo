82560c3dac21c0f89f30fcf8368767f068bb5038
l19891017 <lzq19891017@sina.com>
2019-07-23 16:12:11 +0800

    --销售退货

---
+++ runcase/run.py
@@ -0,0 +1,16 @@
+# coding = utf-8
+import unittest, time, sys
+from BeautifulReport import BeautifulReport
+
+path = 'D:\\airtest_demo\\'
+sys.path.append(path)
+
+test_dir = '../testcase'
+report_dir = '../report'
+test_dir1 = '../smokecase'
+
+discover = unittest.defaultTestLoader.discover(test_dir, pattern='test*.py')
+
+now = time.strftime('%Y-%m-%d %H_%M_%S')
+report_name = report_dir+'/'+now+' test_report.html'
+BeautifulReport(discover).report(filename=report_name, description='jxc Android app test report', report_dir=report_dir)
